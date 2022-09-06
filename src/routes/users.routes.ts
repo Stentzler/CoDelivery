8aa719efb117dedbadede5a1a2d52c429b361ad7
God bless your rotten soul <caionovo10@gmail.com>
2022-09-06 19:25:34 -0300

    feat:route update user

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -2,6 +2,7 @@ import {Router} from 'express';
 
 import { userCreateController } from '../controllers/users/userCreate.controller';
 import { userDeleteController } from '../controllers/users/userDelete.controller';
+import { userEditController } from '../controllers/users/userEdit.controller';
 import { useListController } from '../controllers/users/userList.controller';
 import { schemaValidatedMiddleware } from '../middlewares/schemaValidated.middleware';
 import { userSchema } from '../schemas/users/usersSchema';
@@ -10,6 +11,7 @@ import { userSchema } from '../schemas/users/usersSchema';
 const userRoutes = Router();
 
 userRoutes.post('', schemaValidatedMiddleware(userSchema),userCreateController);
+userRoutes.patch("/:id", userEditController);
 userRoutes.get('', useListController)
 userRoutes.delete('/:id', userDeleteController)
 
--- src/services/Users/userEdit.service.ts
+++ src/services/Users/userEdit.service.ts
@@ -1,6 +1,40 @@
+import AppDataSource from "../../data-source";
+import { Users } from "../../entities/user.entity";
+import { AppError } from "../../errors/AppError";
 
-const userEditService= async () => {
-   
-}
+const userEditService = async (id: string, data: any) => {
+  const userRepository = AppDataSource.getRepository(Users);
 
-export{userEditService}
+  const user = await userRepository.findOne({ where: { id } });
+
+  if (!user) {
+    throw new AppError("user not found", 404);
+  }
+
+  if (typeof data !== "object") {
+    throw new AppError("Request format is not an object", 400);
+  }
+  if (
+    data.id ||
+    data.created_at ||
+    data.updated_at ||
+    data.isRestaurant ||
+    data.isActive||
+    data.address_info.id||
+    data.cart.id||
+    data.payment_info.id||
+    data.cart.price
+  ) {
+    throw new AppError("Those changes are not allowed", 403);
+  }
+  try {
+    data.updated_at = new Date();
+    await userRepository.update(user.id, { ...user, ...data });
+
+    return true;
+  } catch (error) {
+    throw new AppError("Request has invalid properties", 422);
+  }
+};
+
+export { userEditService };
