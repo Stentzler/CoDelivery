0962943009cd39337a08f4825fa8e1ceab91c7e7
God bless your rotten soul <caionovo10@gmail.com>
2022-09-06 16:45:57 -0300

    fix:login schema

--- src/routes/session.routes.ts
+++ src/routes/session.routes.ts
@@ -1,9 +1,11 @@
 import {Router} from 'express';
 import { userLoginController } from '../controllers/session/userLogin.controller';
+import { schemaValidatedMiddleware } from '../middlewares/schemaValidated.middleware';
+import { userLoginSchema } from '../schemas/session/userLoginSchema';
 
 
 const sessionRoutes = Router();
 
-sessionRoutes.post('',userLoginController);
+sessionRoutes.post('',schemaValidatedMiddleware(userLoginSchema),userLoginController);
 
 export default sessionRoutes;
