34052f895d7a3dcc3ef09780915fc136c6f30042
Vinicius Stentzler <vin.stentzler@gmail.com>
2022-09-07 13:51:06 -0300

    hotfix: schema validation name => error

--- src/entities/paymentInfo.entity.ts
+++ src/entities/paymentInfo.entity.ts
@@ -1,24 +1,24 @@
-import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+import {Entity, Column, PrimaryGeneratedColumn} from 'typeorm';
 
-@Entity("paymentInfo")
+@Entity('paymentInfo')
 class PaymentInfo {
-  @PrimaryGeneratedColumn("uuid")
-  readonly id: string;
+	@PrimaryGeneratedColumn('uuid')
+	readonly id: string;
 
-  @Column({ length: 60 })
-  Name: string;
+	@Column({length: 60})
+	Name: string;
 
-  @Column({ length: 20 })
-  cardNo: string;
+	@Column({length: 20})
+	cardNo: string;
 
-  @Column({ length: 10 })
-  cvvNo: string;
+	@Column({length: 10})
+	cvvNo: string;
 
-  @Column({ length: 20 }) //Formato YYYY/MM/DD
-  expireDate: string;
+	@Column({length: 20}) //Formato YYYY/MM/DD
+	expireDate: string;
 
-  @Column({ length: 20 })
-  cpf: string;
+	@Column({length: 20})
+	cpf: string;
 }
 
-export { PaymentInfo };
+export {PaymentInfo};
