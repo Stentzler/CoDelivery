eb2e31ff3eb83056132ba6ec97ea4289c85bcee2
noomloof <79592325+noomloof@users.noreply.github.com>
2022-09-08 10:19:53 -0400

    fix: fixed response message grammar

--- src/controllers/Users/userSoftDelete.controller.ts
+++ src/controllers/Users/userSoftDelete.controller.ts
@@ -6,7 +6,7 @@ const userSoftDeleteController = async (req: Request, res: Response) => {
 
   await userSoftDeleteService(id);
 
-  return res.status(204).json({ message: "User inactivate successfully" });
+  return res.status(204).json({ message: "User deactivated successfully" });
 };
 
 export { userSoftDeleteController };
