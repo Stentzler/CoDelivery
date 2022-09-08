97798dd3d667b53ac1d9a4db13ed033fa63855fe
noomloof <bhtanikawa@gmail.com>
2022-09-08 02:12:16 -0400

    BREAKING CHANGE: reverted changes on src/data-source.ts

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,25 +1,25 @@
-import { DataSource } from "typeorm";
-import "dotenv/config";
+import { DataSource } from 'typeorm';
+import 'dotenv/config';
 
 const AppDataSource = new DataSource(
-  process.env.NODE_ENV === "test"
+  process.env.NODE_ENV === 'test'
     ? {
-        type: "sqlite",
-        database: ":memory:",
+        type: 'sqlite',
+        database: ':memory:',
         synchronize: true,
-        entities: ["src/entities/*.ts"],
+        entities: ['src/entities/*.ts'],
       }
     : {
-        type: process.env.DB_HOST,
-        host: "localhost",
+        type: 'postgres',
+        host: process.env.DB_HOST,
         port: 5432,
         username: process.env.POSTGRES_USER,
         password: process.env.POSTGRES_PWD,
         database: process.env.POSTGRES_DB,
         synchronize: false,
         logging: true,
-        entities: ["src/entities/*.ts"],
-        migrations: ["src/migrations/*.ts"],
+        entities: ['src/entities/*.ts'],
+        migrations: ['src/migrations/*.ts'],
       }
 );
 
