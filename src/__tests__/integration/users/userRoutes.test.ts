258b1f5117802f95d6002425f70726107dbfdbb7
noomloof <bhtanikawa@gmail.com>
2022-09-08 01:54:25 -0400

    feat: added a few more tests

--- src/__tests__/integration/users/userRoutes.test.ts
+++ src/__tests__/integration/users/userRoutes.test.ts
@@ -1,12 +1,27 @@
-import { DataSource } from "typeorm";
-import request from "supertest";
-import AppDataSource from "../../../data-source";
-import app from "../../../app";
-import { mockedUser200 } from "../../mocks";
-import { randomNumberGenerator } from "../../../utils/randomRemover";
+import { DataSource } from 'typeorm';
+import request from 'supertest';
+import AppDataSource from '../../../data-source';
+import app from '../../../app';
+import {
+  editData1,
+  editData2,
+  editData3,
+  fakeToken,
+  mockedUser200,
+  mockedUser201,
+  mockedUser201Login,
+  mockedUserLogin,
+  mockedUserSecond,
+  sensibleDataAddressInfo,
+  sensibleDataPaymentInfo,
+  sensibleDataUser,
+} from '../../mocks';
+import { randomNumberGenerator } from '../../../utils/randomRemover';
 
-describe("/users", () => {
+describe('/users', () => {
   let connection: DataSource;
+  let createdUser201Id = '';
+  let createdUserSecondId = '';
 
   beforeAll(async () => {
     await AppDataSource.initialize()
@@ -14,7 +29,7 @@ describe("/users", () => {
         connection = res;
       })
       .catch((err) => {
-        console.error("Error during Data Source initialization", err);
+        console.error('Error during Data Source initialization', err);
       });
   });
 
@@ -22,43 +37,43 @@ describe("/users", () => {
     await connection.destroy();
   });
 
-  test("POST /users - Should be able to create a user", async () => {
-    const response = await request(app).post("/users").send(mockedUser200);
+  test('POST /users - Should be able to create a user', async () => {
+    const response = await request(app).post('/users').send(mockedUser200);
 
-    expect(response.body).toHaveProperty("id");
-    expect(response.body).toHaveProperty("fullName");
-    expect(response.body).toHaveProperty("userName");
-    expect(response.body).toHaveProperty("email");
-    expect(response.body).toHaveProperty("isRestaurant");
-    expect(response.body).toHaveProperty("isActive");
-    expect(response.body).toHaveProperty("addressInfo");
-    expect(response.body).toHaveProperty("cart");
-    expect(response.body).toHaveProperty("paymentInfo");
-    expect(response.body).toHaveProperty("createdAt");
-    expect(response.body).toHaveProperty("updatedAt");
-    expect(response.body.addressInfo).toHaveProperty("id");
-    expect(response.body.addressInfo).toHaveProperty("address");
-    expect(response.body.addressInfo).toHaveProperty("number");
-    expect(response.body.addressInfo).toHaveProperty("zipCode");
-    expect(response.body.addressInfo).toHaveProperty("city");
-    expect(response.body.addressInfo).toHaveProperty("state");
-    expect(response.body.addressInfo).toHaveProperty("complement");
-    expect(response.body.cart).toHaveProperty("id");
-    expect(response.body.cart).toHaveProperty("price");
-    expect(response.body).not.toHaveProperty("password");
+    expect(response.body).toHaveProperty('id');
+    expect(response.body).toHaveProperty('fullName');
+    expect(response.body).toHaveProperty('userName');
+    expect(response.body).toHaveProperty('email');
+    expect(response.body).toHaveProperty('isRestaurant');
+    expect(response.body).toHaveProperty('isActive');
+    expect(response.body).toHaveProperty('addressInfo');
+    expect(response.body).toHaveProperty('cart');
+    expect(response.body).toHaveProperty('paymentInfo');
+    expect(response.body).toHaveProperty('createdAt');
+    expect(response.body).toHaveProperty('updatedAt');
+    expect(response.body.addressInfo).toHaveProperty('id');
+    expect(response.body.addressInfo).toHaveProperty('address');
+    expect(response.body.addressInfo).toHaveProperty('number');
+    expect(response.body.addressInfo).toHaveProperty('zipCode');
+    expect(response.body.addressInfo).toHaveProperty('city');
+    expect(response.body.addressInfo).toHaveProperty('state');
+    expect(response.body.addressInfo).toHaveProperty('complement');
+    expect(response.body.cart).toHaveProperty('id');
+    expect(response.body.cart).toHaveProperty('subtotal');
+    expect(response.body).not.toHaveProperty('password');
     expect(response.body.isRestaurant).toEqual(false);
     expect(response.body.isActive).toEqual(true);
     expect(response.status).toBe(201);
   });
 
-  test("POST /users - Shouldn't be able to create a user with an already registered email", async () => {
-    const response = await request(app).post("/users").send(mockedUser200);
+  test('POST /users - Should not be able to create a user with an already registered email', async () => {
+    const response = await request(app).post('/users').send(mockedUser200);
 
-    expect(response.body).toHaveProperty("message");
+    expect(response.body).toHaveProperty('message');
     expect(response.status).toBe(400);
   });
 
-  test("POST /users - Shouldn't be able to create a user with missing information 1 - User data", async () => {
+  test('POST /users - Should not be able to create a user with missing information 1 - User data', async () => {
     const newUser = { ...mockedUser200 };
     const value = randomNumberGenerator();
     // Deleting a random property to avoid playing the system
@@ -80,13 +95,13 @@ describe("/users", () => {
         delete newUser.password;
     }
 
-    const response = await request(app).post("/users").send(newUser);
+    const response = await request(app).post('/users').send(newUser);
 
-    expect(response.body).toHaveProperty("message");
+    expect(response.body).toHaveProperty('message');
     expect(response.status).toBe(400);
   });
 
-  test("POST /users - Shouldn't be able to create a user with missing information 2 - User address info", async () => {
+  test('POST /users - Should not be able to create a user with missing information 2 - User address info', async () => {
     const newUser = { ...mockedUser200 };
     const value = randomNumberGenerator();
 
@@ -108,13 +123,13 @@ describe("/users", () => {
         delete newUser.address_info.state;
     }
 
-    const response = await request(app).post("/users").send(newUser);
+    const response = await request(app).post('/users').send(newUser);
 
-    expect(response.body).toHaveProperty("message");
+    expect(response.body).toHaveProperty('message');
     expect(response.status).toBe(400);
   });
 
-  test("POST /users - Shouldn't be able to create a user with missing information 3 - User payment info", async () => {
+  test('POST /users - Should not be able to create a user with missing information 3 - User payment info', async () => {
     const newUser = { ...mockedUser200 };
     const value = randomNumberGenerator();
 
@@ -136,9 +151,216 @@ describe("/users", () => {
         delete newUser.payment_info.expireDate;
     }
 
-    const response = await request(app).post("/users").send(newUser);
+    const response = await request(app).post('/users').send(newUser);
 
-    expect(response.body).toHaveProperty("message");
+    expect(response.body).toHaveProperty('message');
     expect(response.status).toBe(400);
+  });
+
+  test('GET /users/:id - Should list the user', async () => {
+    const createdUser = await request(app).post('/users').send(mockedUser201);
+
+    createdUser201Id = createdUser.body.id;
+
+    const loginResponse = await request(app)
+      .post('/login/users')
+      .send(mockedUser201Login);
+
+    const response = await request(app)
+      .get(`/users/${createdUser201Id}`)
+      .set('Authorization', `Bearer ${loginResponse.body.token}`);
+
+    expect(response.body[0]).toHaveProperty('id');
+    expect(response.body[0]).toHaveProperty('fullName');
+    expect(response.body[0]).toHaveProperty('userName');
+    expect(response.body[0]).toHaveProperty('email');
+    expect(response.body[0]).toHaveProperty('isRestaurant');
+    expect(response.body[0]).toHaveProperty('isActive');
+    expect(response.body[0]).toHaveProperty('addressInfo');
+    expect(response.body[0]).toHaveProperty('cart');
+    expect(response.body[0]).toHaveProperty('paymentInfo');
+    expect(response.body[0]).toHaveProperty('createdAt');
+    expect(response.body[0]).toHaveProperty('updatedAt');
+    expect(response.body[0].addressInfo).toHaveProperty('id');
+    expect(response.body[0].addressInfo).toHaveProperty('address');
+    expect(response.body[0].addressInfo).toHaveProperty('number');
+    expect(response.body[0].addressInfo).toHaveProperty('zipCode');
+    expect(response.body[0].addressInfo).toHaveProperty('city');
+    expect(response.body[0].addressInfo).toHaveProperty('state');
+    expect(response.body[0].addressInfo).toHaveProperty('complement');
+    expect(response.body[0].cart).toHaveProperty('id');
+    expect(response.body[0].cart).toHaveProperty('subtotal');
+    // expect(response.body[0]).not.toHaveProperty('password'); - !!!!!!! Add back later !!!!!!!
+    expect(response.body[0].isRestaurant).toEqual(false);
+    expect(response.body[0].isActive).toEqual(true);
+    expect(response.status).toBe(200);
+  });
+
+  test('GET /users/:id - Should not be able to list a user without a token', async () => {
+    const response = await request(app).get(`/users/${createdUser201Id}`);
+
+    expect(response.body).toHaveProperty('message');
+    expect(response.status).toBe(401);
+  });
+
+  test('GET /users/:id - Should not be able to list a user with an invalid token', async () => {
+    const response = await request(app)
+      .get(`/users/${createdUser201Id}`)
+      .set('Authorization', `Bearer ${fakeToken}`);
+
+    expect(response.body).toHaveProperty('message');
+    expect(response.status).toBe(401);
+  });
+
+  test('GET /users/:id - Should not be able to list a user other than self', async () => {
+    const secondUser = await request(app).post('/users').send(mockedUserSecond);
+
+    createdUserSecondId = secondUser.body.id;
+
+    const loginResponse = await request(app)
+      .post('/login/users')
+      .send(mockedUser201Login);
+
+    const response = await request(app)
+      .get(`/users/${secondUser.body.id}`)
+      .set('Authorization', `Bearer ${loginResponse.body.token}`);
+
+    expect(response.body).toHaveProperty('message');
+    expect(response.status).toBe(401);
+  });
+
+  test('PATCH /users/:id - Should be able to successfully edit a user 1 - User data', async () => {
+    const loginResponse = await request(app)
+      .post('/login/users')
+      .send(mockedUser201Login);
+
+    const response = await request(app)
+      .patch(`/users/${createdUser201Id}`)
+      .set('Authorization', `Bearer ${loginResponse.body.token}`)
+      .send(editData1);
+
+    expect(response.body).toHaveProperty('message');
+    expect(response.status).toBe(200);
+  });
+
+  test('PATCH /users/:id - Should be able to successfully edit a user 2 - User address info', async () => {
+    const loginResponse = await request(app)
+      .post('/login/users')
+      .send(mockedUser201Login);
+
+    const response = await request(app)
+      .patch(`/users/${createdUser201Id}`)
+      .set('Authorization', `Bearer ${loginResponse.body.token}`)
+      .send(editData2);
+
+    expect(response.body).toHaveProperty('message');
+    expect(response.status).toBe(200);
+  });
+
+  test('PATCH /users/:id - Should be able to successfully edit a user 3 - User payment info', async () => {
+    const loginResponse = await request(app)
+      .post('/login/users')
+      .send(mockedUser201Login);
+
+    const response = await request(app)
+      .patch(`/users/${createdUser201Id}`)
+      .set('Authorization', `Bearer ${loginResponse.body.token}`)
+      .send(editData3);
+
+    expect(response.body).toHaveProperty('message');
+    expect(response.status).toBe(200);
+  });
+
+  test('PATCH /users/:id - Should not be able to edit a user without a token', async () => {
+    const response = await request(app)
+      .patch(`/users/${createdUser201Id}`)
+      .send(editData1);
+
+    expect(response.body).toHaveProperty('message');
+    expect(response.status).toBe(401);
+  });
+
+  test('PATCH /users/:id - Should not be able to edit a user with an invalid token', async () => {
+    const response = await request(app)
+      .patch(`/users/${createdUser201Id}`)
+      .set('Authorization', `Bearer ${fakeToken}`)
+      .send(editData1);
+
+    expect(response.body).toHaveProperty('message');
+    expect(response.status).toBe(401);
+  });
+
+  test('PATCH /users/:id - Should not be able to edit a user other than self', async () => {
+    const loginResponse = await request(app)
+      .post('/login/users')
+      .send(mockedUser201Login);
+
+    const response = await request(app)
+      .patch(`/users/${createdUserSecondId}`)
+      .set('Authorization', `Bearer ${loginResponse.body.token}`)
+      .send(editData1);
+
+    expect(response.body).toHaveProperty('message');
+    expect(response.status).toBe(401);
+  });
+
+  test('PATCH /users/:id - Should not be able to edit sensible information 1 - User data', async () => {
+    const value = randomNumberGenerator();
+
+    const loginResponse = await request(app)
+      .post('/login/users')
+      .send(mockedUser201Login);
+
+    const response = await request(app)
+      .patch(`/users/${createdUser201Id}`)
+      .set('Authorization', `Bearer ${loginResponse.body.token}`)
+      .send(sensibleDataUser[value]);
+
+    console.log(sensibleDataUser[value]);
+
+    expect(response.body).toHaveProperty('message');
+    expect(response.status).toBe(403);
+  });
+
+  test('PATCH /users/:id - Should not be able to edit sensible information 2 - User address info', async () => {
+    const loginResponse = await request(app)
+      .post('/login/users')
+      .send(mockedUser201Login);
+
+    const response = await request(app)
+      .patch(`/users/${createdUser201Id}`)
+      .set('Authorization', `Bearer ${loginResponse.body.token}`)
+      .send(sensibleDataAddressInfo);
+
+    expect(response.body).toHaveProperty('message');
+    expect(response.status).toBe(403);
+  });
+
+  test('PATCH /users/:id - Should not be able to edit sensible information 3 - Payment address info', async () => {
+    const loginResponse = await request(app)
+      .post('/login/users')
+      .send(mockedUser201Login);
+
+    const response = await request(app)
+      .patch(`/users/${createdUser201Id}`)
+      .set('Authorization', `Bearer ${loginResponse.body.token}`)
+      .send(sensibleDataPaymentInfo);
+
+    expect(response.body).toHaveProperty('message');
+    expect(response.status).toBe(403);
+  });
+
+  test('PATCH /users/:id - Should not be able to edit sensible information 4 - Cart info', async () => {
+    const loginResponse = await request(app)
+      .post('/login/users')
+      .send(mockedUser201Login);
+
+    const response = await request(app)
+      .patch(`/users/${createdUser201Id}`)
+      .set('Authorization', `Bearer ${loginResponse.body.token}`)
+      .send(sensibleDataPaymentInfo);
+
+    expect(response.body).toHaveProperty('message');
+    expect(response.status).toBe(403);
   });
 });
--- src/__tests__/mocks/index.ts
+++ src/__tests__/mocks/index.ts
@@ -1,24 +1,124 @@
-import { IUserRequest } from "../../interfaces/users";
+import { IUserRequest } from '../../interfaces/users';
 
 export const mockedUser200: IUserRequest = {
-  full_name: "Joana",
-  username: "Joana23",
-  email: "joana@kenzie.com",
-  password: "123456",
+  full_name: 'Joana',
+  username: 'Joana23',
+  email: 'joana@kenzie.com',
+  password: '123456',
   isRestaurant: false,
   address_info: {
-    address: "Rua Barcelos Domingos",
-    number: "20",
-    zipCode: "25017-340",
-    city: "Rio de Janeiro",
-    state: "Rio de Janeiro",
-    complement: "Condomínio Gaivotas",
+    address: 'Rua Barcelos Domingos',
+    number: '20',
+    zipCode: '25017-340',
+    city: 'Rio de Janeiro',
+    state: 'Rio de Janeiro',
+    complement: 'Condomínio Gaivotas',
   },
   payment_info: {
-    name: "Joana",
-    cardNo: "2452374993562234",
-    cvvNo: "950",
-    expireDate: "12/31",
-    cpf: "16203209792",
+    name: 'Joana',
+    cardNo: '2452374993562234',
+    cvvNo: '950',
+    expireDate: '12/31',
+    cpf: '16203209792',
+  },
+};
+
+export const mockedUser201: IUserRequest = {
+  full_name: 'Joana',
+  username: 'Joana23',
+  email: 'joana2@kenzie.com',
+  password: '123456',
+  isRestaurant: false,
+  address_info: {
+    address: 'Rua Barcelos Domingos',
+    number: '20',
+    zipCode: '25017-340',
+    city: 'Rio de Janeiro',
+    state: 'Rio de Janeiro',
+    complement: 'Condomínio Gaivotas',
+  },
+  payment_info: {
+    name: 'Joana',
+    cardNo: '2452374993562234',
+    cvvNo: '950',
+    expireDate: '12/31',
+    cpf: '16203209792',
+  },
+};
+
+export const mockedUserSecond: IUserRequest = {
+  full_name: 'Joana D`Arc',
+  username: 'Joana23',
+  email: 'user@mail.com',
+  password: '1234',
+  isRestaurant: false,
+  address_info: {
+    address: 'Rua Barcelos Domingos',
+    number: '20',
+    zipCode: '25017-342',
+    city: 'Rio de Janeiro',
+    state: 'Rio de Janeiro',
+    complement: 'Condomínio Gaivotas',
+  },
+  payment_info: {
+    name: 'Joana D`Arc',
+    cardNo: '2452374889562234',
+    cvvNo: '951',
+    expireDate: '11/30',
+    cpf: '16203209782',
+  },
+};
+
+export const mockedUserLogin = {
+  email: 'joana@kenzie.com',
+  password: '123456',
+};
+
+export const mockedUser201Login = {
+  email: 'joana2@kenzie.com',
+  password: '123456',
+};
+
+export const fakeToken =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImIyYzQxODhlLTZlMGEtNGFiNi1hZWFjLTQyNDZjNDVkOWMyOCIsImlzUmVzdGF1cmFudCI6ZmFsc2UsImlhdCI6MTY2MjU3MzIzMiwiZXhwIjoxNjYyNjU5NjMyfQ.Q_a9iKQ0QCM6UhMekUYjOAzi647qGEqgylxLaMNlVns';
+
+export const editData1 = {
+  userName: 'Joaninha',
+};
+
+export const editData2 = {
+  addressInfo: {
+    number: '42',
+  },
+};
+
+export const editData3 = {
+  paymentInfo: {
+    cardNo: '2452374889568989',
+  },
+};
+
+export const sensibleDataUser = [
+  { id: 'd3c72f8e-6bd4-2ad5-a92c-2b87c45d9bd9' },
+  { createdAt: '2019-02-05T23:31:22.529Z' },
+  { updatedAt: '2020-12-09T07:48:09.358Z' },
+  { isRestaurant: true },
+];
+
+export const sensibleDataAddressInfo = {
+  addressInfo: {
+    id: 'd3c72f8e-6bd4-2ad5-a92c-2b87c45d9bd9',
+  },
+};
+
+export const sensibleDataPaymentInfo = {
+  paymentInfo: {
+    id: 'd3c72f8e-6bd4-2ad5-a92c-2b87c45d9bd9',
+  },
+};
+
+export const sensibleDataCartInfo = {
+  cart: {
+    price: '3.00',
   },
 };
