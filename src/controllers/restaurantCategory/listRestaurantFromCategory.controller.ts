04076dbc836f59b233b720fd6a337873388d16e5
tierryhahn <tierryhahn@gmail.com>
2022-09-09 09:52:15 -0300

    feat/restaurantCategory

---
+++ src/controllers/restaurantCategory/listRestaurantFromCategory.controller.ts
@@ -0,0 +1,15 @@
+import { listRestaurantFromCategoryService } from "../../services/restaurantCategory/listRestaurantFromCategory.service";
+import { Request, Response } from "express";
+
+const listRestaurantFromCategoryController = async (
+  req: Request,
+  res: Response
+) => {
+  const restaurantId: string = req.params.id_category;
+
+  const listRes = await listRestaurantFromCategoryService(restaurantId);
+
+  return res.status(200).json(listRes);
+};
+
+export { listRestaurantFromCategoryController };
