4c5a18bd9648dc7427cd59bb48c3628f46bd52d2
Vinicius Stentzler <vin.stentzler@gmail.com>
2022-09-12 18:11:43 -0300

    feat: Update user address added

--- src/interfaces/addresses/index.ts
+++ src/interfaces/addresses/index.ts
@@ -1,8 +1,17 @@
 export interface IAddressInfoRequest {
-  street: string;
-  number: string;
-  zipCode: string;
-  city: string;
-  state: string;
-  complement?: string;
+	street: string;
+	number: string;
+	zipCode: string;
+	city: string;
+	state: string;
+	complement?: string;
+}
+
+export interface IAddressUpdateRequest {
+	street?: string;
+	number?: string;
+	zipCode?: string;
+	city?: string;
+	state?: string;
+	complement?: string;
 }
