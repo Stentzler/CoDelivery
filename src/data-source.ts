ded6d51e42ede4559e7ec346b99bd72e677434da
God bless your rotten soul <caionovo10@gmail.com>
2022-09-11 21:16:40 -0300

    deploy 3.7

--- src/data-source.ts
+++ src/data-source.ts
@@ -16,10 +16,10 @@ const AppDataSource = new DataSource(
 				synchronize: false,
 				logging: true,
 				entities:
-					process.env.NODE_ENV === 'production' ? ['dist/entities/*.js'] : ['src/entities/*.ts'],
+					process.env.NODE_ENV === 'production' ? ['dist/src/entities/*.js'] : ['src/entities/*.ts'],
 				migrations:
 					process.env.NODE_ENV === 'production'
-						? ['dist/migrations/*.js']
+						? ['dist/src/migrations/*.js']
 						: ['src/migrations/*.ts'],
 		  }
 );
