c42e402641408132d10a02d0ce47dc5a0ec3aeb8
Vinicius Stentzler <vin.stentzler@gmail.com>
2022-09-12 23:26:13 -0300

    fix: cart routes fixed, subtotal reduce fixed, listProducts from cart added

--- src/routes/cart.routes.ts
+++ src/routes/cart.routes.ts
@@ -1,11 +1,15 @@
 import {Router} from 'express';
 import {addProductController} from '../controllers/cart/addProduct.controller';
 import {deleteCartProductController} from '../controllers/cart/deleteProduct.controller';
+import {listCartProductsController} from '../controllers/cart/listCartProducts.controller';
 import {authenticationMiddleware} from '../middlewares/authentication.middleware';
+import {isUserMiddleware} from '../middlewares/isUser.middlewares';
 import {schemaValidatedMiddleware} from '../middlewares/schemaValidated.middleware';
 import {addProductSchema} from '../schemas/cart/cartSchema';
 
 const cartRoutes = Router();
+
+cartRoutes.get('', authenticationMiddleware, isUserMiddleware, listCartProductsController);
 
 cartRoutes.post(
 	'',
