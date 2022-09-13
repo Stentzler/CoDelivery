e75c3bd90008eb5af24ed33bd4576b5b430b13c4
Victor Cazuriaga <vikktorrv@gmail.com>
2022-09-13 10:20:43 -0300

    fix: interface adresses

--- src/controllers/userAddresses/updateAddress.controller.ts
+++ src/controllers/userAddresses/updateAddress.controller.ts
@@ -1,15 +1,19 @@
-import {Request, Response} from 'express';
-import {IAddressUpdateRequest} from '../../interfaces/addresses';
-import {updateUserAddressService} from '../../services/userAddresses/updateUserAddress.service';
+import { Request, Response } from 'express';
+import { IUserAddressUpdateRequest } from '../../interfaces/addresses';
+import { updateUserAddressService } from '../../services/userAddresses/updateUserAddress.service';
 
 const updateAddressesController = async (req: Request, res: Response) => {
-	const userId: string = req.user.id;
-	const addressId: string = req.params.id;
-	const newAddressInfo: IAddressUpdateRequest = req.body;
+  const userId: string = req.user.id;
+  const addressId: string = req.params.id;
+  const newAddressInfo: IUserAddressUpdateRequest = req.body;
 
-	const updatedUserAddress = await updateUserAddressService(userId, addressId, newAddressInfo);
+  const updatedUserAddress = await updateUserAddressService(
+    userId,
+    addressId,
+    newAddressInfo
+  );
 
-	return res.status(201).json(updatedUserAddress);
+  return res.status(201).json(updatedUserAddress);
 };
 
-export {updateAddressesController};
+export { updateAddressesController };
--- src/services/userAddresses/updateUserAddress.service.ts
+++ src/services/userAddresses/updateUserAddress.service.ts
@@ -1,45 +1,55 @@
 import AppDataSource from '../../data-source';
-import {Users} from '../../entities/user.entity';
-import {UserAddress} from '../../entities/userAddresses.entity';
-import {AppError} from '../../errors/AppError';
-import {IAddressUpdateRequest} from '../../interfaces/addresses';
+import { Users } from '../../entities/user.entity';
+import { UserAddress } from '../../entities/userAddresses.entity';
+import { AppError } from '../../errors/AppError';
+import { IUserAddressUpdateRequest } from '../../interfaces/addresses';
 
 const updateUserAddressService = async (
-	userId: string,
-	addressId: string,
-	newAddressInfo: IAddressUpdateRequest
+  userId: string,
+  addressId: string,
+  newAddressInfo: IUserAddressUpdateRequest
 ) => {
-	const userRepository = AppDataSource.getRepository(Users);
-	const userAddressRepository = AppDataSource.getRepository(UserAddress);
+  const userRepository = AppDataSource.getRepository(Users);
+  const userAddressRepository = AppDataSource.getRepository(UserAddress);
 
-	const user = await userRepository.findOne({where: {id: userId}, relations: {address: true}});
+  const user = await userRepository.findOne({
+    where: { id: userId },
+    relations: { address: true },
+  });
 
-	if (!user) {
-		throw new AppError('User not found', 404);
-	}
+  if (!user) {
+    throw new AppError('User not found', 404);
+  }
 
-	const address = await userAddressRepository.findOneBy({id: addressId});
+  const address = await userAddressRepository.findOneBy({ id: addressId });
 
-	if (!address) {
-		throw new AppError('Address is not registered', 404);
-	}
+  if (!address) {
+    throw new AppError('Address is not registered', 404);
+  }
 
-	const addressBelongsToUser = user.address.find(address => address.id === addressId);
+  const addressBelongsToUser = user.address.find(
+    (address) => address.id === addressId
+  );
 
-	if (!addressBelongsToUser) {
-		throw new AppError('Address does not belong to this user', 401);
-	}
+  if (!addressBelongsToUser) {
+    throw new AppError('Address does not belong to this user', 401);
+  }
 
-	const newAddress: IAddressUpdateRequest = {...address, ...newAddressInfo};
-	delete newAddress.id;
+  const newAddress: IUserAddressUpdateRequest = {
+    ...address,
+    ...newAddressInfo,
+  };
+  delete newAddress.id;
 
-	await userAddressRepository.update(addressId, newAddress);
+  await userAddressRepository.update(addressId, newAddress);
 
-	user.updatedAt = new Date();
+  user.updatedAt = new Date();
 
-	const updatedAddress = await userAddressRepository.findOneBy({id: addressId});
+  const updatedAddress = await userAddressRepository.findOneBy({
+    id: addressId,
+  });
 
-	return updatedAddress;
+  return updatedAddress;
 };
 
-export {updateUserAddressService};
+export { updateUserAddressService };
