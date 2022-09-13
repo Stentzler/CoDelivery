48885ba99f92e598af090dc514619cbdb21cfdec
Victor Cazuriaga <vikktorrv@gmail.com>
2022-09-13 11:22:12 -0300

    fix:verify product condition to check if the product exists in the route to list product by id

--- src/services/session/restaurantLogin.service.ts
+++ src/services/session/restaurantLogin.service.ts
@@ -1,10 +1,10 @@
-import { ISessionLogin } from "../../interfaces/session";
-import AppDataSource from "../../data-source";
-import { AppError } from "../../errors/AppError";
-import * as bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import "dotenv/config";
-import { Restaurant } from "../../entities/restaurant.entity";
+import { ISessionLogin } from '../../interfaces/session';
+import AppDataSource from '../../data-source';
+import { AppError } from '../../errors/AppError';
+import * as bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import 'dotenv/config';
+import { Restaurant } from '../../entities/restaurant.entity';
 
 const restaurantLoginService = async ({ email, password }: ISessionLogin) => {
   const restaurantRepository = AppDataSource.getRepository(Restaurant);
@@ -15,22 +15,22 @@ const restaurantLoginService = async ({ email, password }: ISessionLogin) => {
   );
 
   if (!restaurant) {
-    throw new AppError("Invalid email or password", 403);
+    throw new AppError('Invalid email or password', 403);
   }
   if (restaurant.isActive === false) {
-    throw new AppError("Restaurant is not active", 403);
+    throw new AppError('Restaurant is not active', 403);
   }
 
   const passwordMatch = bcrypt.compareSync(password, restaurant.password);
   if (!passwordMatch) {
-    throw new AppError("Invalid email or password", 403);
+    throw new AppError('Invalid email or password', 403);
   }
 
   const token = jwt.sign(
     { id: restaurant.id, isRestaurant: restaurant.isRestaurant },
     process.env.SECRET_KEY as string,
     {
-      expiresIn: "24h",
+      expiresIn: '24h',
     }
   );
 
