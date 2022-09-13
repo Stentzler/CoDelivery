c598f467cb94010882c5bdcea166360d0a00bc44
noomloof <bhtanikawa@gmail.com>
2022-09-13 14:38:23 -0400

    feat: implemented cart route tests and adjusted routes

--- src/routes/cart.routes.ts
+++ src/routes/cart.routes.ts
@@ -1,23 +1,34 @@
-import {Router} from 'express';
-import {addProductController} from '../controllers/cart/addProduct.controller';
-import {deleteCartProductController} from '../controllers/cart/deleteProduct.controller';
-import {listCartProductsController} from '../controllers/cart/listCartProducts.controller';
-import {authenticationMiddleware} from '../middlewares/authentication.middleware';
-import {isUserMiddleware} from '../middlewares/isUser.middlewares';
-import {schemaValidatedMiddleware} from '../middlewares/schemaValidated.middleware';
-import {addProductSchema} from '../schemas/cart/cartSchema';
+import { Router } from 'express';
+import { addProductController } from '../controllers/cart/addProduct.controller';
+import { deleteCartProductController } from '../controllers/cart/deleteProduct.controller';
+import { listCartProductsController } from '../controllers/cart/listCartProducts.controller';
+import { authenticationMiddleware } from '../middlewares/authentication.middleware';
+import { isUserMiddleware } from '../middlewares/isUser.middlewares';
+import { schemaValidatedMiddleware } from '../middlewares/schemaValidated.middleware';
+import { addProductSchema } from '../schemas/cart/cartSchema';
 
 const cartRoutes = Router();
 
-cartRoutes.get('', authenticationMiddleware, isUserMiddleware, listCartProductsController);
-
-cartRoutes.post(
-	'',
-	authenticationMiddleware,
-	schemaValidatedMiddleware(addProductSchema),
-	addProductController
+cartRoutes.get(
+  '',
+  authenticationMiddleware,
+  isUserMiddleware,
+  listCartProductsController
 );
 
-cartRoutes.delete('/:id', authenticationMiddleware, deleteCartProductController);
+cartRoutes.post(
+  '',
+  authenticationMiddleware,
+  isUserMiddleware,
+  schemaValidatedMiddleware(addProductSchema),
+  addProductController
+);
+
+cartRoutes.delete(
+  '/:id',
+  authenticationMiddleware,
+  isUserMiddleware,
+  deleteCartProductController
+);
 
 export default cartRoutes;
