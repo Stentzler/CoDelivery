2b2856eee79c13b5dd452cfd969a1a849cd13aa1
Victor Cazuriaga <vikktorrv@gmail.com>
2022-09-13 22:16:14 -0300

    fix:category updated

--- src/services/paymentInfo/createPaymentInfo.service.ts
+++ src/services/paymentInfo/createPaymentInfo.service.ts
@@ -1,54 +1,64 @@
-import AppDataSource from "../../data-source"
-import { PaymentInfo } from "../../entities/paymentInfo.entity"
-import { Users } from "../../entities/user.entity"
-import { AppError } from "../../errors/AppError"
-import { IPaymentInfoRequest } from "../../interfaces/paymentInfo"
+import AppDataSource from '../../data-source';
+import { PaymentInfo } from '../../entities/paymentInfo.entity';
+import { Users } from '../../entities/user.entity';
+import { AppError } from '../../errors/AppError';
+import { IPaymentInfoRequest } from '../../interfaces/paymentInfo';
 
-const createPaymentInfoService = async ({userId,name,cardNo,cvvNo,expireDate, cpf}:IPaymentInfoRequest) => {
-    const userRepository = AppDataSource.getRepository(Users);
-    const paymentRepository = AppDataSource.getRepository(PaymentInfo);
+const createPaymentInfoService = async ({
+  userId,
+  name,
+  cardNo,
+  cvvNo,
+  expireDate,
+  cpf,
+}: IPaymentInfoRequest) => {
+  const userRepository = AppDataSource.getRepository(Users);
+  const paymentRepository = AppDataSource.getRepository(PaymentInfo);
 
-console.log(cpf)
-    try {
-        const cpfExists = await paymentRepository.findOne({ where: { cpf } });
-        const userIdValid =await userRepository.findOne({where:{id:userId}})
-     
-       
-   
-        if(!userIdValid){
-   
-            throw new AppError('User not found', 400);
-        }
-    
-        if(userIdValid.paymentInfo!=null  ){
-            if(userIdValid.paymentInfo?.cpf !=""||userIdValid.paymentInfo?.expireDate !=""||userIdValid.paymentInfo?.cvvNo !=""||userIdValid.paymentInfo?.cardNo !=""||userIdValid.paymentInfo?.name !=""){
+  try {
+    const cpfExists = await paymentRepository.findOne({ where: { cpf } });
+    const userIdValid = await userRepository.findOne({ where: { id: userId } });
 
-                throw new AppError("you already have a payment method, you can edit or delete it and create a new one",404) 
-            }
-        }
-   
-       if(cpfExists){
-           throw new AppError('cpf is already being used', 400);
-       }
-   
-       const newPayment= new PaymentInfo()
-       newPayment.name=name
-       newPayment.cardNo=cardNo
-       newPayment.cvvNo=cvvNo
-       newPayment.expireDate=expireDate
-       newPayment.cpf=cpf
-   
-       paymentRepository.create(newPayment)
-       await paymentRepository.save(newPayment)
-   
-       await userRepository.update(userId, { paymentInfo:newPayment })
-       return true 
-    } catch (error) {
-        if(error instanceof Error){
-            throw new AppError(error.message, 400);
-        }
+    if (!userIdValid) {
+      throw new AppError('User not found', 400);
     }
-   
-}
 
-export{createPaymentInfoService}
+    if (userIdValid.paymentInfo != null) {
+      if (
+        userIdValid.paymentInfo?.cpf != '' ||
+        userIdValid.paymentInfo?.expireDate != '' ||
+        userIdValid.paymentInfo?.cvvNo != '' ||
+        userIdValid.paymentInfo?.cardNo != '' ||
+        userIdValid.paymentInfo?.name != ''
+      ) {
+        throw new AppError(
+          'you already have a payment method, you can edit or delete it and create a new one',
+          404
+        );
+      }
+    }
+
+    if (cpfExists) {
+      throw new AppError('cpf is already being used', 400);
+    }
+
+    const newPayment = new PaymentInfo();
+    newPayment.name = name;
+    newPayment.cardNo = cardNo;
+    newPayment.cvvNo = cvvNo;
+    newPayment.expireDate = expireDate;
+    newPayment.cpf = cpf;
+
+    paymentRepository.create(newPayment);
+    await paymentRepository.save(newPayment);
+
+    await userRepository.update(userId, { paymentInfo: newPayment });
+    return true;
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new AppError(error.message, 400);
+    }
+  }
+};
+
+export { createPaymentInfoService };
