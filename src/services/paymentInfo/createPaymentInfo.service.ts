50261ef2ce17c5a3834d38b82ab81db4824f0100
noomloof <bhtanikawa@gmail.com>
2022-09-14 09:50:44 -0400

    fix: adjusted response statuses and implemented a few tests

--- src/services/paymentInfo/createPaymentInfo.service.ts
+++ src/services/paymentInfo/createPaymentInfo.service.ts
@@ -17,22 +17,33 @@ const createPaymentInfoService = async ({
 
   try {
     const cpfExists = await paymentRepository.findOne({ where: { cpf } });
-    const idValid = await userRepository.findOne({ where: { id: id } ,relations:{paymentInfo:true}});
+    const idValid = await userRepository.findOne({
+      where: { id: id },
+      relations: { paymentInfo: true },
+    });
 
     if (!idValid) {
-      throw new AppError('User not found', 400);
+      throw new AppError('User not found', 404);
     }
 
     if (idValid.paymentInfo !== null) {
-      if (idValid.paymentInfo?.cpf !== '' ||idValid.paymentInfo?.expireDate !== '' ||idValid.paymentInfo?.cvvNo !== '' ||idValid.paymentInfo?.cardNo !== '' ||idValid.paymentInfo?.name !== '') {
-        throw new AppError('You already have a payment method, you can edit or delete it and create a new one', 404);
+      if (
+        idValid.paymentInfo?.cpf !== '' ||
+        idValid.paymentInfo?.expireDate !== '' ||
+        idValid.paymentInfo?.cvvNo !== '' ||
+        idValid.paymentInfo?.cardNo !== '' ||
+        idValid.paymentInfo?.name !== ''
+      ) {
+        throw new AppError(
+          'You already have a payment method, you can edit or delete it and create a new one',
+          409
+        );
       }
     }
 
     if (cpfExists) {
-      throw new AppError('Cpf is already being used', 400);
+      throw new AppError('Cpf is already being used', 409);
     }
-
     const newPayment = new PaymentInfo();
     newPayment.name = name;
     newPayment.cardNo = cardNo;
