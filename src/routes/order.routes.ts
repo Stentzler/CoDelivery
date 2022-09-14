5b7af99766d6e841740f6cab80857c9018726ff5
God bless your rotten soul <caionovo10@gmail.com>
2022-09-14 12:01:59 -0300

    feat:initiating deletion of order

--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { createOrderController } from "../controllers/orders/createOrder.controller";
+import { deleteOrderController } from "../controllers/orders/deleteOrder.controller";
 import { authenticationMiddleware } from "../middlewares/authentication.middleware";
+import { idVerifierMiddleware } from "../middlewares/idVerifier.middleware";
 
 
 const orderRoutes = Router()
 
 orderRoutes.post('', authenticationMiddleware, createOrderController)
+orderRoutes.delete('/:id',authenticationMiddleware,deleteOrderController)
 
 
 export default orderRoutes;
---
+++ src/services/orders/deleteOrder.service.ts
@@ -0,0 +1,41 @@
+import AppDataSource from "../../data-source";
+import { Order } from "../../entities/order.entity";
+import { Restaurant } from "../../entities/restaurant.entity";
+import { Users } from "../../entities/user.entity";
+import { AppError } from "../../errors/AppError";
+
+const deleteOrderService = async (userId: string, id: string, isRestaurant:boolean) => {
+
+  const orderRepository = AppDataSource.getRepository(Order);
+  const restaurantRepository=AppDataSource.getRepository(Restaurant)
+  const userRepository = AppDataSource.getRepository(Users);
+
+  const order = await orderRepository.findOne({ where: { id:id }, relations: {user:true}});
+  if(!order){
+    throw new AppError('Order not found', 404)
+  }
+  
+  if(isRestaurant){
+      const restaurant= await restaurantRepository.findOne({ where: { id:userId }})
+      if(!restaurant){
+        throw new AppError('Restaurant not found', 404)
+      }
+      if(restaurant.id!==order.restaurant.id){
+
+    }
+    }else{
+      const user= await userRepository.findOne({ where: { id:userId }})
+         if(!user){
+            throw new AppError('User not found', 404)
+         }
+         if(user.id!==order.user.id){
+
+         }
+  }
+
+  
+
+  console.log(order);
+};
+
+export { deleteOrderService };
